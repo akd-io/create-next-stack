2ce1e7073c18714586901a03fb1f09508320e3d1
Anders Kjær Damgaard <akd@akd.io>
2021-06-29 05:23:32 +0200

    Add TODO in setupStyledComponents

--- src/setup/steps/setup-styled-components.ts
+++ src/setup/steps/setup-styled-components.ts
@@ -2,4 +2,6 @@ import Command from "@oclif/command"
 
 export async function setupStyledComponents(this: Command): Promise<void> {
   this.log("Setting up styled-components...")
+
+  // TODO: Implement setupStyledComponents
 }
