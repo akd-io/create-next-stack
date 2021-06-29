17ba4b667639dabdab6d704238f05211a6d0d497
Anders Kjær Damgaard <akd@akd.io>
2021-06-29 05:34:57 +0200

    Add Emotion support

--- src/setup/steps/setup-emotion.ts
+++ src/setup/steps/setup-emotion.ts
@@ -1,5 +1,69 @@
 import Command from "@oclif/command"
+import execa from "execa"
+import fs from "fs/promises"
+import { throwError } from "../../error-handling"
+import { isUnknownObject } from "../../helpers/is-unknown-object"
 
 export async function setupEmotion(this: Command): Promise<void> {
   this.log("Setting up Emotion...")
+
+  try {
+    await execa("yarn add @emotion/react @emotion/styled")
+    await execa("yarn add --dev @emotion/babel-plugin")
+
+    const babelrcString = await fs.readFile(".babelrc", "utf8")
+    const babelConfig = JSON.parse(babelrcString)
+
+    if (!isUnknownObject(babelConfig)) {
+      throw new TypeError("Expected babelConfig to be an object.")
+    }
+
+    // Add css-prop support as per Next.js-specific Emotion docs: https://emotion.sh/docs/css-prop#babel-preset
+    if (!Array.isArray(babelConfig.presets)) {
+      throw new TypeError("Expected babelConfig.presets to be an array.")
+    }
+    const presets = babelConfig.presets
+    if (!Array.isArray(presets[0])) {
+      throw new TypeError("Expected babelConfig.presets[0] to be an array.")
+    }
+    if (presets[0][0] !== "next/babel") {
+      throw new TypeError(
+        `Expected babelConfig.presets[0][0] to be "next/babel".`
+      )
+    }
+    const nextBabelPresetTuple = presets[0]
+    if (!isUnknownObject(nextBabelPresetTuple[1])) {
+      throw new TypeError("Expected babelConfig.presets[0][1] to be an object.")
+    }
+    const presetNameToConfigMap = nextBabelPresetTuple[1]
+    presetNameToConfigMap["preset-react"] = {
+      runtime: "automatic",
+      importSource: "@emotion/react",
+    }
+
+    // Add Emotion's Babel plugin as per their docs: https://emotion.sh/docs/install#babelrc
+    if (Array.isArray(babelConfig.plugins)) {
+      babelConfig.plugins = ["@emotion", ...babelConfig.plugins]
+    } else {
+      babelConfig.plugins = ["@emotion"]
+    }
+
+    const newBabelrcString = JSON.stringify(babelConfig)
+    await fs.writeFile(".babelrc", newBabelrcString)
+
+    // Add TypeScript support for the css-prop as per the docs: https://emotion.sh/docs/typescript#css-prop
+    const tsConfigString = await fs.readFile("tsconfig.json", "utf8")
+    const tsConfig = JSON.parse(tsConfigString)
+    if (!isUnknownObject(tsConfig)) {
+      throw new TypeError("Expected tsConfig to be an object.")
+    }
+    if (!isUnknownObject(tsConfig.compilerOptions)) {
+      throw new TypeError("Expected tsConfig.compilerOptions to be an object.")
+    }
+    tsConfig.compilerOptions.jsxImportSource = "@emotion/react"
+    const newTSConfigString = JSON.stringify(tsConfig)
+    await fs.writeFile("tsconfig.json", newTSConfigString)
+  } catch (error) {
+    throwError.call(this, "An error occurred while setting up Emotion.", error)
+  }
 }
