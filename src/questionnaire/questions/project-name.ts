344183e316de6b9d0dc899a34e22421dd5ecc37f
Anders Kjær Damgaard <akd@akd.io>
2021-06-29 02:25:45 +0200

    Refactor promptProjectName

--- src/questionnaire/questions/project-name.ts
+++ src/questionnaire/questions/project-name.ts
@@ -2,32 +2,26 @@ import inquirer from "inquirer"
 import path from "path"
 import { validateProjectName } from "../../helpers/validate-project-name"
 
-const answerName = "projectPath"
-
-type ProjectNameAnswers = {
-  [answerName]: string
-}
-
 export const promptProjectName = async () => {
-  const { projectPath } = await inquirer.prompt<ProjectNameAnswers>([
-    {
-      name: answerName,
-      type: "input",
-      message: "What is your project named?",
-      validate: (projectName) => {
-        if (typeof projectName !== "string") return false
+  const answerName = "projectName"
+  type ProjectNameAnswers = {
+    [answerName]: string
+  }
+  const { projectName } = await inquirer.prompt<ProjectNameAnswers>({
+    name: answerName,
+    type: "input",
+    message: "What is your project named?",
+    validate: (projectName) => {
+      if (typeof projectName !== "string") return false
 
-        const trimmedInput = projectName.trim()
+      const trimmedInput = projectName.trim()
 
-        if (trimmedInput.length <= 0) return false
+      if (trimmedInput.length <= 0) return false
 
-        return validateProjectName(projectName).valid
-      },
+      return validateProjectName(projectName).valid
     },
-  ])
+  })
 
-  const resolvedProjectPath = path.resolve(projectPath)
-  const projectName = path.basename(resolvedProjectPath)
-
-  return projectName
+  const resolvedProjectPath = path.resolve(projectName)
+  return path.basename(resolvedProjectPath)
 }
