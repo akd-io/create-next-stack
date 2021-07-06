40104b7292dbe37b2424c7e2b382c24bcdfd42d0
Anders Kjær Damgaard <akd@akd.io>
2021-07-07 01:08:33 +0200

    Refactor addReadmeStep

--- src/setup/steps/add-readme.ts
+++ src/setup/steps/add-readme.ts
@@ -52,44 +52,42 @@ function generateScriptTableRows(this: Command, answers: QuestionnaireAnswers) {
   type ScriptTableRow = {
     name: string
     description: string
-    include: boolean
+    filter?: boolean
   }
 
   const scripts: ScriptTableRow[] = [
     {
       name: /* md */ `\`dev\``,
       description: /* md */ `Runs the Next.js development server.`,
-      include: true,
     },
     {
       name: /* md */ `\`build\``,
       description: /* md */ `Generates a production build.`,
-      include: true,
     },
     {
       name: /* md */ `\`start\``,
       description: /* md */ `Runs the Next.js production server built using \`build\` script.`,
-      include: true,
     },
     {
       name: /* md */ `\`lint\``,
       description: /* md */ `Runs [ESLint](https://eslint.org/) to catch linting errors in the source code.`,
-      include: true,
     },
     {
       name: /* md */ `\`format\``,
       description: /* md */ `Runs Prettier to format the source code.`,
-      include: setUpPrettierStep.shouldRun(answers),
+      filter: setUpPrettierStep.shouldRun(answers),
     },
     {
       name: /* md */ `\`prepare\``,
       description: /* md */ `The [\`prepare\` life cycle script](https://docs.npmjs.com/cli/v7/using-npm/scripts#life-cycle-scripts) is used to set up Git pre-commit hooks when people run \`yarn install\`. This script should not be run manually.`,
-      include: setUpPrettierStep.shouldRun(answers),
+      filter: setUpPrettierStep.shouldRun(answers),
     },
   ]
 
   const scriptRowsString = scripts
-    .filter((script) => script.include)
+    .filter((script) =>
+      typeof script.filter !== "undefined" ? script.filter : true
+    )
     .map((script) => `|${script.name}|${script.description}|`)
     .join("\n")
 
@@ -103,59 +101,56 @@ function generateTechnologyTableRows(
   type TechnologyTableRow = {
     name: string
     links: string
-    include: boolean
+    filter?: boolean
   }
 
   const technologies: TechnologyTableRow[] = [
     {
       name: /* md */ `[Next.js](https://nextjs.org/)`,
       links: /* md */ `[Docs](https://nextjs.org/docs) - [Learn Next.js](https://nextjs.org/learn) - [GitHub repo](https://github.com/vercel/next.js)`,
-      include: true,
     },
     {
       name: /* md */ `[React](https://reactjs.org/)`,
       links: /* md */ `[Docs](https://reactjs.org/docs/getting-started.html) - [GitHub repo](https://github.com/facebook/react)`,
-      include: true,
     },
     {
       name: /* md */ `[TypeScript](https://www.typescriptlang.org/)`,
       links: /* md */ `[Docs](https://www.typescriptlang.org/docs/) - [GitHub repo](https://github.com/microsoft/TypeScript)`,
-      include: true,
     },
     {
       name: /* md */ `[Emotion](https://emotion.sh/docs/introduction)`,
       links: /* md */ `[Docs](https://emotion.sh/docs/introduction) - [GitHub repo](https://github.com/emotion-js/emotion)`,
-      include: setUpEmotionStep.shouldRun(answers),
+      filter: setUpEmotionStep.shouldRun(answers),
     },
     {
       name: /* md */ `[ESLint](https://eslint.org/)`,
       links: /* md */ `[Configuration](https://eslint.org/docs/user-guide/configuring/) - [Rules](https://eslint.org/docs/rules/) - [GitHub Repo](https://github.com/eslint/eslint)`,
-      include: true,
     },
     {
       name: /* md */ `[Prettier](https://prettier.io/)`,
       links: /* md */ `[Docs](https://prettier.io/docs/en/index.html) - [Options](https://prettier.io/docs/en/options.html) - [GitHub repo](https://github.com/prettier/prettier)`,
-      include: setUpPrettierStep.shouldRun(answers),
+      filter: setUpPrettierStep.shouldRun(answers),
     },
     {
       name: /* md */ `[Husky](https://typicode.github.io/husky/)`,
       links: /* md */ `[Docs](https://typicode.github.io/husky/) - [GitHub repo](https://github.com/typicode/husky)`,
-      include: setUpLintStagedStep.shouldRun(answers),
+      filter: setUpLintStagedStep.shouldRun(answers),
     },
     {
       name: /* md */ `[lint-staged](https://github.com/okonet/lint-staged)`,
       links: /* md */ `[GitHub repo](https://github.com/okonet/lint-staged)`,
-      include: setUpLintStagedStep.shouldRun(answers),
+      filter: setUpLintStagedStep.shouldRun(answers),
     },
     {
       name: /* md */ `[Yarn](https://yarnpkg.com/)`,
       links: /* md */ `[CLI Docs](https://yarnpkg.com/cli) - [GitHub repo](https://github.com/yarnpkg/berry)`,
-      include: true,
     },
   ]
 
   const technologyRowsString = technologies
-    .filter((technology) => technology.include)
+    .filter((technology) =>
+      typeof technology.filter !== "undefined" ? technology.filter : true
+    )
     .map((technology) => `|${technology.name}|${technology.links}|`)
     .join("\n")
 
