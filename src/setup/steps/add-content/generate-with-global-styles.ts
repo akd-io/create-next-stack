3b0fbe7a460253104cefb452cef4bf6eb036410b
Anders Kjær Damgaard <akd@akd.io>
2021-07-11 04:47:55 +0200

    Refactor generateWithDefaultGlobalStyles to refer to globalStyles const

--- src/setup/steps/add-content/generate-with-global-styles.ts
+++ src/setup/steps/add-content/generate-with-global-styles.ts
@@ -1,30 +1,11 @@
+import { globalStyles } from "./global-styles"
+
 export const generateWithDefaultGlobalStyles = (): string => /* tsx */ `
 import { css, Global } from "@emotion/react";
 import React from "react";
 
 const globalStyles = css\`
-  html,
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-
-  code {
-    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-      DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-  }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-    font-weight: bold;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
+${globalStyles}
 \`;
 
 const WithDefaultGlobalStyles: React.FC = ({ children }) => {
