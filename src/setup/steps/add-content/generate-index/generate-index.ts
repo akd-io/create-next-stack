5be149cd5cbfe5da6c0e753d11bca9a167770f30
Anders Kjær Damgaard <akd@akd.io>
2021-07-16 05:05:23 +0200

    Fix generate-index styling technology check

--- src/setup/steps/add-content/generate-index/generate-index.ts
+++ src/setup/steps/add-content/generate-index/generate-index.ts
@@ -1,12 +1,19 @@
 import { QuestionnaireAnswers } from "../../../../questionnaire/questionnaire"
 import { techValues } from "../../../../questionnaire/questions/technologies"
-import { generateIndexWithCssModules } from "./generate-index-with-css-modules"
+import { generateIndexWithCssInJs } from "./with-css-in-js/generate-index-with-css-in-js"
 import { generateIndexWithCssModules } from "./with-css-modules/generate-index-with-css-modules"
 
 export const generateIndex = (answers: QuestionnaireAnswers) => {
-  if (answers.technologies.includes(techValues.emotion)) {
-    return generateIndexWithEmotion(answers)
-  } else {
+  if (
+    answers.technologies.includes(techValues.emotion) ||
+    answers.technologies.includes(techValues.styledComponents)
+  ) {
+    return generateIndexWithCssInJs(answers)
+  } else if (answers.technologies.includes(techValues.cssModules)) {
     return generateIndexWithCssModules(answers)
+  } else {
+    throw new Error(
+      "Unsupported styling technology found in generateIndex, or no styling technology was chosen."
+    )
   }
 }
