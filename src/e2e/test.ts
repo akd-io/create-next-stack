e10399c1ee40dbbd562f8a35ac96e2d0a09eb3f5
Anders Kjær Damgaard <akd@akd.io>
2021-07-19 07:46:37 +0200

    Add Git name and email to fix git commit error during create-next-app

--- src/e2e/test.ts
+++ src/e2e/test.ts
@@ -1,8 +1,18 @@
 import console from "console"
+import execa from "execa"
 import util from "util"
 import { testDefaultOptions } from "./tests/default-options"
 ;(async () => {
   try {
+    // Set Git name and email, so Git commit doesn't fail during create-next-app
+    await execa("git", ["config", "--global", "user.name", `"Test user"`])
+    await execa("git", [
+      "config",
+      "--global",
+      "user.email",
+      `"test-user@create-next-stack.com"`,
+    ])
+
     await testDefaultOptions()
   } catch (error) {
     if (util.types.isNativeError(error)) {
