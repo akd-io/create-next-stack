fff0f58caabd56d23d82d826aea73f0efe22eb0f
Anders Kjær Damgaard <akd@akd.io>
2021-07-19 05:16:51 +0200

    Add error logging to test.ts

--- src/e2e/test.ts
+++ src/e2e/test.ts
@@ -1,8 +1,17 @@
+import console from "console"
+import { isNativeError } from "util/types"
 import { testDefaultOptions } from "./tests/default-options"
 ;(async () => {
   try {
     await testDefaultOptions()
   } catch (error) {
+    if (isNativeError(error)) {
+      console.error(error.message)
+    } else if (typeof error === "string") {
+      console.error(error)
+    } else {
+      console.error("Error: An error of unknown type was thrown.")
+    }
     process.exit(1)
   }
 })()
