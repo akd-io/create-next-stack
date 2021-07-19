475ce44ecadd8b2c414e8f188b37d4c71f539991
Anders Kjær Damgaard <akd@akd.io>
2021-07-19 08:00:23 +0200

    Only set user.name and user.email if not set already

--- src/e2e/test.ts
+++ src/e2e/test.ts
@@ -4,14 +4,30 @@ import util from "util"
 import { testDefaultOptions } from "./tests/default-options"
 ;(async () => {
   try {
-    // Set Git name and email, so Git commit doesn't fail during create-next-app
-    await execa("git", ["config", "--global", "user.name", `"Test user"`])
-    await execa("git", [
-      "config",
-      "--global",
-      "user.email",
-      `"test-user@create-next-stack.com"`,
-    ])
+    // If not done already, Set Git name and email so `git commit` doesn't fail during create-next-app
+    await execa("git", ["config", "--global", "user.name"])
+      .then((result) => {
+        console.log(`user.name found: ${result.stdout}`)
+      })
+      .catch(async () => {
+        console.log(`user.name didn't exist. Setting user.name="Test user"`)
+        await execa("git", ["config", "--global", "user.name", "Test user"])
+      })
+    await execa("git", ["config", "--global", "user.email"])
+      .then((result) => {
+        console.log(`user.email found: ${result.stdout}`)
+      })
+      .catch(async () => {
+        console.log(
+          `user.email didn't exist. Setting user.email="test-user@create-next-stack.com"`
+        )
+        await execa("git", [
+          "config",
+          "--global",
+          "user.email",
+          "test-user@create-next-stack.com",
+        ])
+      })
 
     await testDefaultOptions()
   } catch (error) {
