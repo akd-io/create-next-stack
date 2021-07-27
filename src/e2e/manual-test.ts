6c7c3dc879e012b508012b5a29ec332ff9e87a23
Anders Damgaard <akd@akd.io>
2021-07-27 19:32:16 +0200

    Add checkFormattingLintingBuild to manual-test

--- src/e2e/manual-test.ts
+++ src/e2e/manual-test.ts
@@ -1,14 +1,18 @@
 import execa from "execa"
 import { v4 as uuidv4 } from "uuid"
+import { checkFormattingLintingBuild } from "./helpers/check-formatting-linting-build"
 import { logTestInfo } from "./helpers/test-logging"
 ;(async () => {
   const projectName = uuidv4()
+  const runDirectory = `../create-next-stack-tests/${projectName}`
 
   logTestInfo(`Running Create Next Stack...`)
-  await execa("./bin/run-prod", [`../create-next-stack-tests/${projectName}`], {
+  await execa("./bin/run-prod", [runDirectory], {
     stdio: "inherit",
   })
 
+  await checkFormattingLintingBuild(runDirectory)
+
   logTestInfo(`Open in vscode with:`)
-  logTestInfo(`    code ../create-next-stack-tests/${projectName}`)
+  logTestInfo(`    code ${runDirectory}`)
 })()
