10adcd349018ddd7baf41c16cc31c20263f974a9
Anders Damgaard <akd@akd.io>
2021-07-29 18:19:20 +0200

    Catch and log errors in manual-test.ts

--- src/e2e/manual-test.ts
+++ src/e2e/manual-test.ts
@@ -3,27 +3,32 @@ import endent from "endent"
 import execa from "execa"
 import { v4 as uuidv4 } from "uuid"
 import { checkFormattingLintingBuild } from "./helpers/check-formatting-linting-build"
+import { exitWithError } from "./helpers/exit-with-error"
 import { logTestInfo } from "./test-logging"
 ;(async () => {
-  const projectName = uuidv4()
-  const runDirectory = `../create-next-stack-tests/${projectName}`
+  try {
+    const projectName = uuidv4()
+    const runDirectory = `../create-next-stack-tests/${projectName}`
 
-  const command = "./bin/run-prod"
-  const args = [...process.argv.slice(2), runDirectory]
+    const command = "./bin/run-prod"
+    const args = [...process.argv.slice(2), runDirectory]
 
-  logTestInfo(`Running command: ${command} ${args.join(" ")}`)
+    logTestInfo(`Running command: ${command} ${args.join(" ")}`)
 
-  await execa(command, args, { stdio: "inherit" })
+    await execa(command, args, { stdio: "inherit" })
 
-  await checkFormattingLintingBuild(runDirectory)
+    await checkFormattingLintingBuild(runDirectory)
 
-  logTestInfo("")
-  logTestInfo(endent`
+    logTestInfo("")
+    logTestInfo(endent`
     ${chalk.green("Test successful!")}
 
     To open the project in vscode, run:
 
         ${chalk.cyan(`code ${runDirectory}`)}
   `)
-  logTestInfo("")
+    logTestInfo("")
+  } catch (error) {
+    exitWithError(error)
+  }
 })()
