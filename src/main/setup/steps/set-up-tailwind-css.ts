54700fc882ccadac70b5235e61385fb305c4a667
Anders Damgaard <akd@akd.io>
2021-09-03 10:54:52 +0200

    Add tailwind config generation

--- src/main/setup/steps/set-up-tailwind-css.ts
+++ src/main/setup/steps/set-up-tailwind-css.ts
@@ -1,3 +1,5 @@
+import endent from "endent"
+import { promises as fs } from "fs"
 import { install, packages } from "../packages"
 import { Step } from "../step"
 
@@ -14,5 +16,25 @@ export const setUpTailwindCssStep: Step = {
       flags["package-manager"],
       { dev: true }
     )
+
+    await addTailwindConfig()
   },
+}
+
+const addTailwindConfig = async () => {
+  const tailwindConfigString = endent/* js */ `
+    module.exports = {
+      mode: 'jit',
+      purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+      darkMode: false, // or 'media' or 'class'
+      theme: {
+        extend: {},
+      },
+      variants: {
+        extend: {},
+      },
+      plugins: [],
+    }
+  `
+  await fs.writeFile("tailwind.config.js", tailwindConfigString)
 }
