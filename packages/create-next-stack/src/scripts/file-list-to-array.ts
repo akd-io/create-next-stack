b5f206b5a897410dc10ceb93662a08b289912e4b
Anders Kjær Damgaard <akd@akd.io>
2023-05-29 01:27:23 +0200

    Fix file paths not resolving on windows

--- packages/create-next-stack/src/scripts/file-list-to-array.ts
+++ packages/create-next-stack/src/scripts/file-list-to-array.ts
@@ -1,6 +1,11 @@
-// This script reads each line of stdin and outputs a JSON array of the lines.
-// This is useful for passing a list of files to a command.
-// Example usage: `ls | node file-list-to-array.ts`
+import { resolve } from "path"
+
+/*
+This script reads each line of stdin and outputs a JSON array of the lines.
+This is useful for passing a list of files to a command.
+Example usage: `ls | node file-list-to-array.ts`
+*/
+
 let input = ""
 process.stdin.on("data", (data) => {
   input += data.toString()
@@ -8,7 +13,7 @@ process.stdin.on("data", (data) => {
 process.stdin.on("end", () => {
   const filePaths = input.split("\n").filter(Boolean)
   const files = filePaths.map((filePath) => ({
-    filePath: filePath,
+    filePath: filePath.replace(resolve(process.cwd()) + "/", ""),
     fileName: filePath.split("/").pop()?.split(".")[0] || filePath,
   }))
   console.log(JSON.stringify(files))
