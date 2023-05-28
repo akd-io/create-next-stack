12e5867912b97418c5acd0c3c742c2fbbd91a278
Anders Kjær Damgaard <akd@akd.io>
2023-05-29 00:27:32 +0200

    Update test scripts and ci

---
+++ packages/create-next-stack/src/scripts/file-list-to-array.ts
@@ -0,0 +1,15 @@
+// This script reads each line of stdin and outputs a JSON array of the lines.
+// This is useful for passing a list of files to a command.
+// Example usage: `ls | node file-list-to-array.ts`
+let input = ""
+process.stdin.on("data", (data) => {
+  input += data.toString()
+})
+process.stdin.on("end", () => {
+  const filePaths = input.split("\n")
+  const files = filePaths.map((filePath) => ({
+    filePath: filePath,
+    fileName: filePath.split("/").pop()?.split(".")[0] || filePath,
+  }))
+  console.log(JSON.stringify(files))
+})
