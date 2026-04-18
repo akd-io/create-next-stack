6df3d5293864054966c0d6130c2707880066e607
Anders Kjær Damgaard <akd@akd.io>
2026-04-18 21:40:14 +0200

    refactor: make Anchor polymorphic via polymorphic()
    - Mantine's recommended pattern
    - drops PolymorphicComponentProps type helper in favor of wrapper

--- apps/website/components/Anchor.tsx
+++ apps/website/components/Anchor.tsx
@@ -3,10 +3,11 @@
 import {
   Anchor as MantineAnchor,
   AnchorProps as MantineAnchorProps,
-  PolymorphicComponentProps,
+  polymorphic,
 } from "@mantine/core"
 
-type AnchorProps = PolymorphicComponentProps<"a", MantineAnchorProps>
-export const Anchor = (props: AnchorProps) => {
-  return <MantineAnchor c="#319bff" fw="bold" {...props} />
-}
+export const Anchor = polymorphic<"a", MantineAnchorProps>(
+  (props: MantineAnchorProps) => {
+    return <MantineAnchor c="#319bff" fw="bold" {...props} />
+  },
+)
